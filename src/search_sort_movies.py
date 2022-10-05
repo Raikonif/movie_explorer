bf313c475940e4c050f968702846f1b922ae3613
Raikonif <raikonif@gmail.com>
2022-10-05 13:13:37 -0400

    search_short_movies.py: list_movies_desc_asc modified now get all correct data needed, in the last key of the dict for return a list into the value

--- src/search_sort_movies.py
+++ src/search_sort_movies.py
@@ -11,6 +11,33 @@ def search_files_csv(path):
     return files
 
 
+def search_and_open_file_if_exist(files):
+    for file in files:
+        concat_dir = path + '/' + file
+        if os.path.exists(concat_dir):
+            with open(concat_dir, 'r', encoding='utf-8') as file_name:
+                reader = csv.reader(file_name)
+        return reader
+
+
+def search_movie_title_typing(input_user):
+    files = search_files_csv(path)
+    # reader = search_and_open_file_if_exist(files)
+    for file in files:
+        concat_dir = path + '/' + file
+        if os.path.exists(concat_dir):
+            with open(concat_dir, 'r', encoding='utf-8') as file_name:
+                reader = csv.reader(file_name)
+                for row in reader:
+                    if input_user in row[1]:
+                        print(row)
+                return row
+
+
+def search_movie_by_release_dates(input_user):
+    pass
+
+
 def list_movies_desc_asc(path, input_user):
     # path = '../movie_files'
     files = search_files_csv(path)
@@ -33,22 +60,26 @@ def list_movies_desc_asc(path, input_user):
                     str_row = reversed_row.replace(' ', ',', 1).replace(']', '').replace("'", '').replace('"', '')
                     str_row = str_row[::-1]
                     row_formatted = str_row.strip().split(',')
+                    row_formatted[2] = row_formatted[2].replace('|', ',').split(',')
                     dict_movie = {
-                               'movieId': row_formatted[0],
-                               'title': row_formatted[1],
-                               'genres': row_formatted[2]
-                            }
+                        'movieId': row_formatted[0],
+                        'title': row_formatted[1],
+                        'genres': row_formatted[2]
+                    }
                     list_of_dict_movies.append(dict_movie)
                 if input_user == 1:
                     list_of_dict_movies.sort(key=lambda m: m['title'])
                 elif input_user == 2:
                     list_of_dict_movies.sort(key=lambda m: m['title'], reverse=True)
 
-                list_print = [print(dict_movie) for dict_movie in list_of_dict_movies]
-                return list_print
+                # list_print = [print(dict_movie) for dict_movie in list_of_dict_movies]
+                print(list_of_dict_movies)
+                return list_of_dict_movies
 
 
 if __name__ == '__main__':
-    # input_user = 'Toy Story'
+    input_user = 'Toy Story'
     path = '../movie_files'
     list_movies_desc_asc(path, 1)
+
+    # search_movie_title_typing(input_user)
