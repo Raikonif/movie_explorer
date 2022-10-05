9397a9e9c53d88b52d9f7b9babac95d1385c8066
Raikonif <raikonif@gmail.com>
2022-10-05 09:24:42 -0400

    fun_working_search

--- src/search_sort_movies.py
+++ src/search_sort_movies.py
@@ -16,20 +16,21 @@ def list_movies_desc_asc(path, input_user):
     files = search_files_csv(path)
     list_of_dict_movies = []
     list_sorted = []
-    print(files)
+    # print(files)
     for file in files:
         # print(file)
         concat_dir = path + '/' + file
         if os.path.exists(concat_dir):
-            print(concat_dir)
+            # print(concat_dir)
+            print('Please Wait...')
             with open(concat_dir, 'r', encoding='utf-8') as file_name:
                 file_char_replaced = [line.replace(',', ' ') for line in file_name]
                 reader = csv.reader(file_char_replaced, delimiter=',')
                 for row in reader:
                     str_row = str(row)
-                    first_replace = str_row.replace(' ', ',', 1).replace('[', '')
+                    first_replace = str_row.replace(' ', ',', 1).replace('[', '').replace("'", '').replace('"', '')
                     reversed_row = first_replace[::-1]
-                    str_row = reversed_row.replace(' ', ',', 1).replace(']', '')
+                    str_row = reversed_row.replace(' ', ',', 1).replace(']', '').replace("'", '').replace('"', '')
                     str_row = str_row[::-1]
                     row_formatted = str_row.strip().split(',')
                     dict_movie = {
@@ -38,33 +39,13 @@ def list_movies_desc_asc(path, input_user):
                                'genres': row_formatted[2]
                             }
                     list_of_dict_movies.append(dict_movie)
+                if input_user == 1:
                     list_of_dict_movies.sort(key=lambda m: m['title'])
-                    # print(list_of_dict_movies.append(dict_movie))
-                list_print = [print(dict) for dict in list_of_dict_movies]
+                elif input_user == 2:
+                    list_of_dict_movies.sort(key=lambda m: m['title'], reverse=True)
+
+                list_print = [print(dict_movie) for dict_movie in list_of_dict_movies]
                 return list_print
-                # for line in file_name.readlines()[1:]:
-                #     line_formatted = line.strip().s(',')
-                #     # line_formatted = line_formatted.remove(',')
-                #     print(line_formatted)
-                #     # if line_formatted[1].startswith('"') and line_formatted[1].endswith('"'):
-                #     #     print(line_formatted)
-                #
-                # #     dict_movies = {
-                # #         'movieId': line_formatted[0],
-                # #         'title': line_formatted[1],
-                # #         'genres': line_formatted[2]
-                # #     }
-                # #     list_of_dict_movies.append(dict_movies)
-                # if input_user == 1:
-                #     list_of_dict_movies.sort(key=lambda m: m['title'], reverse=True)
-                # elif input_user == 2:
-                #     list_of_dict_movies.sort(key=lambda m: m['title'], reverse=False)
-                # #
-                # list_print = [print(dict) for dict in list_of_dict_movies]
-                # return list_of_dict_movies
-                # if data is not None:
-                #     pass
-                # print(data)
 
 
 if __name__ == '__main__':
