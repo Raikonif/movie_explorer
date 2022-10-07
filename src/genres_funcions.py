a2c2a3e2cc24ef46e206105c3524f54656c38d29
Israel Zurita Sanchez <israelzs16.iz@gmail.com>
2022-10-07 13:43:44 -0400

    genres with Jason output

--- src/genres_funcions.py
+++ src/genres_funcions.py
@@ -1,71 +1,52 @@
-import os
-import csv
+from core_functions import data_management
+import uuid
+from save_responses import save_responses
+
+def get_genres():
+    filter_gen = list()
+    movies_list = data_management()
+    movies_list.pop(0)
+    for movie in movies_list :
+        for genre in movie['genres']:
+            if not genre in filter_gen and genre[0].isupper(): 
+                filter_gen.append(genre) 
+    genres_list = list(set(filter_gen))
+    genres_list.sort()
+    filter_gen.sort()
+    return filter_gen 
 
 
-def count_titles_by_genre(asc=None):
-    absolute_path = os.path.dirname(__file__)
-    full_path = absolute_path.replace('src', 'movie_files')
+
+def create_obj(data):
+    response_dic = {}
+    response_dic['request_id'] = str(uuid.uuid4())
+    response_dic['data'] = data
+    response_dic['size'] = len(data) 
+    save_responses(response_dic,response_dic['request_id']) 
+
+
+def count_titles_by_genre(asc = None):
     genres_Dict = {}
     count = 0
-    genres = sort_by_genre()
+    movies_list = data_management()
+    movies_list.pop(0)
+    genres = get_genres()
     for element in genres:
         genres_Dict.update({element: count})
-    print(type(genres_Dict))
-    files = search_files_csv(full_path)
-    concat_dir = full_path + '/' + files
-    with open(concat_dir, 'r', encoding='utf-8') as movies_csv:
-        csv_reader = csv.reader(movies_csv)
-        rows = list(csv_reader)
-        rows.pop(0)
-        for row in rows:
-            for genre in row[2].split('|'):
-                if genre in genres_Dict:
-                    genres_Dict[genre] += 1
-    if asc == 'asc':
-        sorted_dict_by_value = dict(sorted(genres_Dict.items(), key=lambda item: item[1]))
-        return sorted_dict_by_value
+    for row in movies_list :
+        for genre in row['genres']:
+            if genre in genres_Dict:
+                genres_Dict[genre]+=1   
+    if  asc == 'asc':                
+        genres_Dict = dict(sorted(genres_Dict.items(), key=lambda item: item[1]))                   
     elif asc == 'desc':
-        sorted_dict_by_value = dict(sorted(genres_Dict.items(), key=lambda item: item[1], reverse=True))
-        return sorted_dict_by_value
-
+        genres_Dict = dict(sorted(genres_Dict.items(), key=lambda item: item[1],reverse = True))                
+    create_obj(genres_Dict)                       
     return genres_Dict
 
 
-def search_files_csv(path):
-    with os.scandir(path) as entries:
-        files = [f.name for f in entries if f.name.endswith('movies.csv')]
-        listToStr = ''.join([str(elem) for elem in files])
-    return listToStr
-
-
 def sort_by_genre():
-    absolute_path = os.path.dirname(__file__)
-    full_path = absolute_path.replace('src', 'movie_files')
-    files = search_files_csv(full_path)
-    concat_dir = full_path + '/' + files
-    # print(path)
-    # print( concat_dir)
-    with open(concat_dir, 'r', encoding='utf-8') as movies_csv:
-        filter_gen = list()
-        csv_reader = csv.reader(movies_csv)
-        # print(type(csv_reader))
-        rows = list(csv_reader)
-        rows.pop(0)
-        for row in rows:
-            for genre in row[2].split('|'):
-                if not genre in filter_gen:  # if dic has genre askey
-                    filter_gen.append(genre)  # dic en key +1
-        # genres_list = list(set(filter_gen))
-        # genres_list.sort()
-        filter_gen.sort()
-    return filter_gen
-
-
-# print(search_files_csv('./movie_files'))
-# print(type(search_files_csv('./movie_files')))
-# print(sort_by_genre(full_path, 'Romance'))
-
-if __name__ == '__main__':
-    input_user = 'Toy Story'
-    # path = './movie_files'
-    # list_movies_desc_asc(path, 1)
+    filter_gen = get_genres()
+    create_obj(filter_gen)
+    return filter_gen 
+            
