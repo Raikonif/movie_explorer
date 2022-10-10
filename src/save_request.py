5f11cdecfaa48141a619df12417c4782b523ba74
RenanDev3 <renan_cb@hotmail.com>
2022-10-10 17:13:04 -0300

    fix the save requests file

--- src/save_request.py
+++ src/save_request.py
@@ -1,17 +1,18 @@
-import uuid
+import uuid, ast
 from datetime import datetime as dt
 import csv
 
 RELATIVE_PATH_TO_SAVE_REQUESTS = 'saved_requests/requests.csv'
 
-
 def save_request(request_args):
+
     unique_identifier = uuid.uuid4()
     time_stamp = dt.strftime(dt.now(), '%Y-%m-%d %H:%M:%S')
-    request_args = [(key, value) for key, value in request_args.items() if value is not None]
-    request_args_to_plain_text = [' '.join(str(item)) for item in request_args]
-    return unique_identifier
+    request_args = [['--'+key, value] for key,value in request_args.items() if value is not None]
+    request_args_to_plain_text = str(request_args).replace('[','').replace(']', '').replace(':', '').replace('\'', '').replace(',','')
 
     with open(RELATIVE_PATH_TO_SAVE_REQUESTS, 'a', encoding='UTF-8', newline='') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow([unique_identifier, request_args_to_plain_text, time_stamp])
+
+    return unique_identifier
